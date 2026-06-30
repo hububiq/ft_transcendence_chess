b7d86efbc97ce92ee299f25c0bd01259ca3f6afe
jkalinow <jacek.kalinowski@wp.pl>
2026-06-30 17:12:09 +0200

    remove duplicates game and tounament

--- fastapi_backend/models.py
+++ fastapi_backend/models.py
@@ -2,17 +2,6 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
 
-class Game(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    
-    # Soft links to Django Users
-    white_player_id: int
-    black_player_id: Optional[int] = None # Nullable for playing against AI
-    
-    # Game data
-    status: str = Field(default="ongoing") # ongoing, completed, drawn
-    winner_id: Optional[int] = Nonefrom sqlmodel import SQLModel, Field
-
 # ---------------------------------------------------------
 # 1. TOURNAMENTS
 # ---------------------------------------------------------
@@ -71,11 +60,3 @@ class GameInvitation(SQLModel, table=True):
     
     # Timestamp
     played_at: datetime = Field(default_factory=datetime.utcnow)
-
-# 2. THE TOURNAMENTS TABLE (Simple MVP version)
-class Tournament(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    creator_id: int
-    status: str = Field(default="waiting")
-    winner_id: Optional[int] = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
