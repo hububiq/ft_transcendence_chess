a108ac42297f7a69d36c2dae3c23fcc89ef71e7f
Milos Mitkovic <mitkovic_m@hotmail.com>
2026-06-30 13:00:18 +0200

    fix: data fetching enviroment file

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -7,8 +7,6 @@ export const api = axios.create({
 });
 
 // Interceptor
-// right before ANY request leaves the browse, this function
-// intercepts it and slaps the JWT token onto the header automatically.
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
   if (token && config.headers) {
@@ -16,5 +14,3 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
-// No more need to manually type headers: { Authorization: ... } in codebase ever again.
