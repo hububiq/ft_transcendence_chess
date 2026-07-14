959e6d53d644379a55c05f3cff2b68d0d764e8a5
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-07-15 01:11:16 +0200

    fix: game_id info exchange
    add games.py with endpoint for creating game_id and generating new database row
    readapt server.py for human vs human game
    rebuild matchmaking.py to insert row to database with new game after match is made, sending payload to React

---
+++ fastapi_backend/api/games.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from database import async_session
+from models import Game
+from pydantic import BaseModel
+
+router = APIRouter()
+
+class BotMatchRequest(BaseModel):
+    user_id: int
+
+@router.post("/api/games/vs-bot/")
+async def start_bot_game(request: BotMatchRequest):
+    async with async_session() as session:
+        new_game = Game(
+            white_player_id=request.user_id,
+            black_player_id=None,
+            status="ongoing"
+        )
+        
+        session.add(new_game)
+        await session.commit()
+        await session.refresh(new_game)
+
+        return {"game_id": new_game.id}
